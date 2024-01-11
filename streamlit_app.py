a3a13a570757bb272889a7334117618ff95288b7
supriyar14 <156312088+supriyar14@users.noreply.github.com>
2024-01-11 18:32:29 +0530

    Update streamlit_app.py
    added breakfast

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
-streamlit.title('my parents now healthy food')
+streamlit.title('My parents  healthy food')
 streamlit.header('Breakfast')
-streamlit.text('oats,fruits,juice')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
