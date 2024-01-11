3eb972e475f9e766fe5654caa991f19a692ffe4b
supriyar14 <156312088+supriyar14@users.noreply.github.com>
2024-01-11 18:31:05 +0530

    Update streamlit_app.py
    added menu for breakfast

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit
-streamlit.title('my parents now healthy dinner')
+streamlit.title('my parents now healthy food')
+streamlit.header('Breakfast')
+streamlit.text('oats,fruits,juice')
+
