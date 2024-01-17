2af0704c1c6f230be3a1426b756a00bf2c4c5c2b
supriyar14 <156312088+supriyar14@users.noreply.github.com>
2024-01-17 23:02:35 +0530

    Update streamlit_app.py
    added user interface

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,8 @@ streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
 #new fetch from api
 import requests
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 streamlit.text(fruityvice_response.json())
 streamlit.header("Fruityvice Fruit Advice!")
