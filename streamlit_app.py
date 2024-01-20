903996bc44f6d4544970029d08f3cbdb8a22f188
supriyar14 <156312088+supriyar14@users.noreply.github.com>
2024-01-21 00:46:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
 #new fetch from api
 #adding def define what get_fruityvice_data is going to mean
-def get_fruityvice_data (this_fruit_choice)
+def get_fruityvice_data (this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
